535723d00981afbac17618596b8f9b893ed1729c
wangshuang <wangshuang@em3.ai>
2021-11-16 16:07:09 +0800

    feat: 构造结构体返回响应

--- user_api/api/user.go
+++ user_api/api/user.go
@@ -8,8 +8,10 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"mxshop_api/user_api/global/response"
 	"mxshop_api/user_api/proto"
 	"net/http"
+	"time"
 )
 
 func HandleGrpcErrorToHttp(err error, c *gin.Context) {
@@ -65,12 +67,14 @@ func GetUserList(ctx *gin.Context) {
 	}
 	result := make([]interface{}, 0)
 	for _, value := range rsp.Data {
-		data := make(map[string]interface{})
-		data["id"] = value.Id
-		data["name"] = value.NickName
-		data["birthday"] = value.BirthDay
-		data["gender"] = value.Mobile
-		result = append(result, data)
+		user := response.UserResponse{
+			Id:       value.Id,
+			NickName: value.NickName,
+			Birthday: time.Time(time.Unix(int64(value.BirthDay), 0)),
+			Gender:   value.Gender,
+			Mobile:   value.Mobile,
+		}
+		result = append(result, user)
 	}
 	ctx.JSON(http.StatusOK, result)
 }
