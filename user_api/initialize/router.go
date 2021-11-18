ada1077fec225014caeab2fff0d16eb11c5536b3
wangshuang <wangshuang@em3.ai>
2021-11-18 11:40:13 +0800

    feat: 配置跨域

--- user_api/initialize/router.go
+++ user_api/initialize/router.go
@@ -2,11 +2,16 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"mxshop_api/user_api/middlewares"
 	router2 "mxshop_api/user_api/router"
 )
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
+
+	// 配置跨域
+	Router.Use(middlewares.Cors())
+
 	ApiGroup := Router.Group("/u/v1")
 	router2.InitUserRouter(ApiGroup)
 	return Router
