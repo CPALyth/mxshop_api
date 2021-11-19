11a09417003fa4b44a35a9a731b3ad740f08230f
wangshuang <wangshuang@em3.ai>
2021-11-19 17:19:09 +0800

    feat: 健康检查

--- user_api/initialize/router.go
+++ user_api/initialize/router.go
@@ -4,10 +4,18 @@ import (
 	"github.com/gin-gonic/gin"
 	"mxshop_api/user_api/middlewares"
 	router2 "mxshop_api/user_api/router"
+	"net/http"
 )
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
+
+	Router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"success": true,
+		})
+	})
 
 	// 配置跨域
 	Router.Use(middlewares.Cors())
