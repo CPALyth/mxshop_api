c9ecd5a018a61272f2b73e1b0dc56882be45ff7b
wangshuang <wangshuang@em3.ai>
2021-11-23 15:58:59 +0800

    feat: gin集成nacos

--- user_api/initialize/config.go
+++ user_api/initialize/config.go
@@ -1,9 +1,13 @@
 package initialize
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/fsnotify/fsnotify"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 	"mxshop_api/user_api/global"
 )
 
@@ -15,17 +19,56 @@ func InitConfig() {
 	if err := v.ReadInConfig(); err != nil {
 		panic(err)
 	}
-	if err := v.Unmarshal(global.ServerConfig); err != nil {
+	if err := v.Unmarshal(global.NacosConfig); err != nil {
 		panic(err)
 	}
+	zap.S().Infof("配置信息: &v", global.NacosConfig)
+	// 服务端配置
+	sc := []constant.ServerConfig{
+		{
+			IpAddr: global.NacosConfig.Host,
+			Port:   global.NacosConfig.Port,
+		},
+	}
+	// 客户端配置
+	cc := constant.ClientConfig{
+		NamespaceId:         global.NacosConfig.Namespace,
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "tmp/nacos/log",
+		CacheDir:            "tmp/nacos/cache",
+		RotateTime:          "1h",
+		MaxAge:              3,
+		LogLevel:            "debug",
+	}
+	// 创建动态配置客户端
+	configClient, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": sc,
+		"clientConfig":  cc,
+	})
+	if err != nil {
+		panic(err)
+	}
+	// 读取配置
+	content, err := configClient.GetConfig(vo.ConfigParam{
+		DataId: global.NacosConfig.DataId,
+		Group:  global.NacosConfig.Group,
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal([]byte(content), &global.ServerConfig)
+	if err != nil {
+		zap.S().Fatalf("读取nacos配置文件失败: %s", err.Error())
+	}
 	fmt.Println(global.ServerConfig)
-
-	// viper动态监控配置文件变化
-	v.WatchConfig()
-	v.OnConfigChange(func(e fsnotify.Event) {
-		fmt.Println("config file changed:", e.Name)
-		_ = v.ReadInConfig()
-		_ = v.Unmarshal(global.ServerConfig)
-		fmt.Println(global.ServerConfig)
+	// 监听配置变化
+	err = configClient.ListenConfig(vo.ConfigParam{
+		DataId: global.NacosConfig.DataId,
+		Group:  global.NacosConfig.Group,
+		OnChange: func(namespace, group, dataId, data string) {
+			fmt.Println("配置文件产生变化")
+			fmt.Println("group:" + group + ", dataId:" + dataId + ", data:" + data)
+		},
 	})
 }
